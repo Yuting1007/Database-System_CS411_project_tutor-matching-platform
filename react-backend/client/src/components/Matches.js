c3e530b4a0dc51f58124f23e2317da01ed0faca6
Nanyi Yang <nanyiyang@Nanyis-MacBook-Pro.local>
2020-04-09 22:44:08 -0700

    working on matches

--- react-backend/client/src/components/Matches.js
+++ react-backend/client/src/components/Matches.js
@@ -22,23 +22,121 @@ class Matches extends Component {
         super(props);
         console.log(sessionStorage)
         this.state = {
-            students: []
+            students: [],
+            tutors: [],
+            tutorMatches: [],
+            userID: null
         };
+
+        this.onStudentToTutorMatchClick = this.onStudentToTutorMatchClick.bind();
+        this.getMatches = this.getMatches.bind();
     }
 
     
   
     async componentDidMount() {
-      fetch('/matches/all-students')
+    //   fetch('/matches/all-students')
+    //     .then(res => {
+    //       console.log(res);
+    //       return res.json()
+    //     })
+    //     .then(students => {
+    //       console.log(students);
+    //       this.setState({ students })
+    //     });
+
+        fetch('/matches/all-tutors')
         .then(res => {
           console.log(res);
           return res.json()
         })
-        .then(students => {
-          console.log(students);
-          this.setState({ students })
+        .then(tutors => {
+          console.log(tutors);
+          this.setState({ tutors })
         });
+        console.log(sessionStorage)
+        this.setState({
+            userID: sessionStorage.getItem("s_id")
+        })
+        //console.log("userID" + this.state.userID)
+
+        this.getMatches();
     }
+
+    getMatches = () => {
+        let tutorIds = [];
+        console.log("getting matches");
+        let url = '/matches/show-tutor-matches/' + this.state.userID
+      fetch(url)
+      .then(res => res.json())
+      .then(matches => {
+        
+        for (let x of matches) {
+            tutorIds.push(x['t_id'])
+        }
+        console.log('tutor ids: ' + tutorIds);
+        let myTutorMatches = []
+        for (let id of tutorIds)  {
+            for (let x of this.state.tutors) {
+                if (x['t_id'] == id) {
+                    myTutorMatches.push(x);
+                    break;
+                }
+            }
+        }
+        console.log('tutor matches here: ' + myTutorMatches)
+        //now remove duplicates
+        let uniqueTutors = new Set(myTutorMatches);
+        let uniqueTutorMatches = [...uniqueTutors];
+        console.log('unique tutors: ' + uniqueTutorMatches)
+        this.setState({
+            tutorMatches: uniqueTutorMatches
+        })
+      })
+      
+    }
+
+    onStudentToTutorMatchClick = (e) => {
+        // console.log("click");
+        console.log(e.target.id);
+        console.log(this.state.userID)
+        // should first check if match exists !!!!!!!!!!!!!!!!!!!!!!!!!!
+        
+
+        let newMatch = {
+            t_id: e.target.id,
+            s_id: this.state.userID,
+            m_s_like: 1,
+            m_t_like: 0
+           }
+        console.log(newMatch)
+           
+          //POST req here
+          const requestOptions = {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({newMatch})
+          };
+        fetch('/matches/match-create', requestOptions);
+
+        // let targetTutor = e.target.id;
+        // console.log("tutors: " + this.state.tutors);
+        // for (let x of this.state.tutors) {
+        //     console.log(x.t_id)
+        //     if (x.t_id === targetTutor) {
+        //         console.log("found tutor");
+        //         let updatedTutors = this.state.tutors;
+        //         updatedTutors.push(x);
+        //         this.setState({
+        //             tutorMatches: updatedTutors
+        //         })
+        //         break;
+        //     }
+        // }
+        //this.getMatches();
+        console.log("tutorMatches: " +this.state.tutorMatches)
+        //call refresh
+      };
 
     render() {
         return (
@@ -55,20 +153,45 @@ class Matches extends Component {
                         </Row>
                     </Container>
                 </Jumbotron>
+                <Button onClick={this.getMatches}>Show/Refresh Matches!</Button>
                 <Table striped className="matches-table">
                     <thead>
-                        <th>ID</th>
-                        <th>Name</th>
-                        <th>Age</th>
-                        <th>Location</th>
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Age</th>
+                            <th>Location</th>
+                        </tr>
                     </thead>
                     <tbody>
-                    {this.state.students.map(student => 
-                        <tr key={student.s_id}>
-                            <td classname="id-col">{student.s_id}</td>
-                            <td>{student.s_name}</td>
-                            <td>{student.s_age}</td>
-                            <td>{student.s_location}</td>
+                    {this.state.tutorMatches.map(tutor => 
+                        <tr key={tutor.t_id}>
+                            <td className="id-col">{tutor.t_id}</td>
+                            <td>{tutor.t_name}</td>
+                            <td>{tutor.t_age}</td>
+                            <td>{tutor.t_location}</td>
+                            <td><Button id={tutor.t_id} onClick={this.onStudentToTutorMatchClick}>Unmatch!</Button></td>
+                        </tr>
+                    )}
+                    </tbody>
+                </Table>
+                <Table striped className="tutors-table">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Age</th>
+                            <th>Location</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    {this.state.tutors.map(tutor => 
+                        <tr key={tutor.t_id}>
+                            <td className="id-col">{tutor.t_id}</td>
+                            <td>{tutor.t_name}</td>
+                            <td>{tutor.t_age}</td>
+                            <td>{tutor.t_location}</td>
+                            <td><Button id={tutor.t_id} onClick={this.onStudentToTutorMatchClick}>Match!</Button></td>
                         </tr>
                     )}
                     </tbody>
--- react-backend/routes/matches.js
+++ react-backend/routes/matches.js
@@ -21,16 +21,82 @@ router.get("/all-students", (req, res) => {
   const queryString = "SELECT * FROM students";
   connection.query(queryString, (err, rows, fields) => {
     if (err) {
-      console.log("Failed to query for users: " + err);
+      console.log("Failed to query for students: " + err);
       res.sendStatus(500);
       //res.end();
       return;
     }
 
-    console.log("I think we fetched users successfully")
+    console.log("I think we fetched students successfully")
     console.log(rows)
     res.json(rows)
   })
+})
+
+router.get("/all-tutors", (req, res) => {
+    const connection = getConnection();
+  
+    const queryString = "SELECT * FROM tutors";
+    connection.query(queryString, (err, rows, fields) => {
+      if (err) {
+        console.log("Failed to query for tutors: " + err);
+        res.sendStatus(500);
+        //res.end();
+        return;
+      }
+  
+      console.log("I think we fetched tutors successfully")
+      console.log(rows)
+      res.json(rows)
+    })
+  })
+
+  ////
+// GET request that returns t_id of all tutors a student has liked
+////
+router.get('/show-tutor-matches/:id', (req, res) => {
+    console.log("Fetching tutor with id: " + req.params.id)
+  
+    const connection = getConnection();
+  
+    const userId = req.params.id;
+    const queryString = "SELECT t_id FROM matches WHERE s_id = ?";
+    connection.query(queryString, [userId], (err, rows, fields) => {
+      if (err) {
+        console.log("Failed to query for student: " + err);
+        res.sendStatus(500);
+        return;
+      }
+  
+      console.log("Fetched tutor matches successfully")
+      console.log(rows)
+      res.json(rows)
+    })
+  
+    //res.end()
+  })
+
+  router.post('/match-create', (req, res) => {
+    console.log("Trying to create a new match...")
+    console.log(typeof req.body.newMatch.s_id)
+    console.log(req.body)
+    const s_id = parseInt(req.body.newMatch.s_id);
+    const t_id = parseInt(req.body.newMatch.t_id);
+    const m_s_like = parseInt(req.body.newMatch.m_s_like);
+    const m_t_like = parseInt(req.body.newMatch.m_t_like)
+    
+    const queryString = 'INSERT INTO matches (s_id, t_id, m_s_like, m_t_like) VALUES (?, ?, ?, ?)'
+    
+    getConnection().query(queryString, [s_id, t_id, m_s_like, m_t_like], (err, results, fields) => {
+        if (err) {
+            console.log("Failed to insert new student-to-tutor match: " + err)
+            res.sendStatus(500)
+            return
+        }
+
+        console.log('Inserted a new match')
+        res.end()
+    })
 })
 
 
