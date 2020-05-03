92c0182d31e18e74ce3634d40afc207ac4526da3
Nanyi Yang <nanyiyang@Nanyis-MacBook-Pro.local>
2020-05-02 21:17:08 -0700

    testing pt2

--- react-backend/app.js
+++ react-backend/app.js
@@ -35,7 +35,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use(express.static(path.join(__dirname, 'public')));  //THIS IS THE ORIGINAL
+//app.use(express.static(path.join(__dirname, 'public')));  //THIS IS THE ORIGINAL
+app.use('/', express.static(path.join(__dirname, '/client/build')));
 
 // // ================================= ADDITIONAL PORTION BEGIN =====================================
 // // Serve static files from the React frontend app
