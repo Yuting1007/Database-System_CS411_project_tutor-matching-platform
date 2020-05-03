f2b5e715094c66bffc20ee4a1ebdfb3cf829b230
IvanZzz <ivan.nzhong@gmail.com>
2020-05-03 12:28:51 -0500

    mongodb additional resource function initial completion. Might need improvement

--- react-backend/routes/addition.js
+++ react-backend/routes/addition.js
@@ -22,6 +22,10 @@ router.post("/", function(req, res){
         var level = req.body.level;
         var course = req.body.course;
         
+    console.log("major is: " + major)
+    console.log("level is: " + level)
+    console.log("course is: " + course)
+
         // var item = function(major, level, course, next){
         //     Addition.find({major: major, level: level, course: course},{link: 1}, function(err, items){
         //         if(err){
@@ -47,7 +51,14 @@ router.post("/", function(req, res){
             if(err){
                 console.log(err);
             } else {
+                //items.toObject();
+                console.log("reach this point 1")
+                console.log(items)
+                console.log("reach this point 2")
                 res.send(pagelist(items));
+                console.log("reach this point 3")
+                console.log(items)
+                console.log("reach this point 4")
             }
         })
     }
@@ -56,11 +67,42 @@ router.post("/", function(req, res){
 function pagelist(items) {
     result = "<html><body><ul>";
     items.forEach(function(item) {
-      itemstring = "<li>" +  '<a href="'+ item.link + '">'+ item.link +'</a>'+ "</li>";
-      result = result + itemstring;
+        itemstring = "<li>" +  '<a href="'+ item.major + '">'+ item.major +'</a>'+ "</li>";
+        // let itemObject = item.toObject();
+        // itemstring = "<li>" +  '<a href="'+ itemObject.link + '">'+ itemObject.link +'</a>'+ "</li>";
+        console.log("string is: " + itemstring)
+        result = result + itemstring;
     });
     result = result + "</ul></body></html>";
     return result;
   }
+
+  router.post("/get-resources", (req, res) => {
+    
+    var addi_pre_major = req.body.formResults.addi_pre_major;
+    var addi_pre_level = req.body.formResults.addi_pre_level;
+    var addi_pre_course = req.body.formResults.addi_pre_course;
+
+    console.log("front end major is: " + addi_pre_major)
+    console.log("front end level is: " + addi_pre_level)
+    console.log("front end course is: " + addi_pre_course)
+
+    //var major_condition = 'major: ' + "'" + addi_pre_major + "'";
+    //var addi_pre_course = "{$ne: '411'}"
+
+    Addition.find({major: addi_pre_major, level: addi_pre_level, course: addi_pre_course},  {link: 1, major: 1, level: 1, course: 1, description: 1}).lean().exec(function(err, items){
+        if(err){
+            console.log(err);
+        } else {
+            //items.toObject();
+            //res.json(pagelist(items));
+            res.json(items);
+            console.log("reach this point 1")
+            console.log(items)
+            console.log("reach this point 2")
+        }
+    })
+    
+})
 
   module.exports = router;
