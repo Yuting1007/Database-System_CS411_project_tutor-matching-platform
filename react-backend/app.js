34dc4193ee7fe8da0571938ab5494fc71e39dbd1
Nanyi Yang <nanyiyang@Nanyis-MacBook-Pro.local>
2020-05-03 23:31:42 -0700

    idk

--- react-backend/app.js
+++ react-backend/app.js
@@ -24,7 +24,7 @@ mongoose.connect("mongodb://localhost:27017/addition",  {useNewUrlParser: true, 
 var app = express();
 app.use(express.static(path.join(__dirname, '/client/build')))
 
--app.get('/', function (req, res) {
+-app.get('/', function (req, res) {})
 +app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
   });
