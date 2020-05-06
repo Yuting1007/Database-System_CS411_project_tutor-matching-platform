428e89ddf0c94ef85d3d8661b965f99b53bde369
Nanyi Yang <nanyiyang@Nanyis-MacBook-Pro.local>
2020-05-05 21:02:15 -0700

    connection

--- react-backend/routes/index.js
+++ react-backend/routes/index.js
@@ -20,37 +20,38 @@ function getConnection() {
     })
 }
 
-// var db_config = {
-//   host: 'us-cdbr-east-06.cleardb.net',
-//     user: 'ba0144eebe0617',
-//     password: '45188a1d', 
-//     database: 'heroku_195486945502404'
-// };
+var db_config = {
+  host: 'us-cdbr-east-06.cleardb.net',
+    user: 'ba0144eebe0617',
+    password: '45188a1d', 
+    database: 'heroku_195486945502404'
+};
 
-// var safeConnection;
+var safeConnection;
 
-// function handleDisconnect() {
-//   safeConnection = mysql.createConnection(db_config); // Recreate the connection, since
-//                                                   // the old one cannot be reused.
+function handleDisconnect() {
+  console.log('CONNECTING')
+  safeConnection = mysql.createConnection(db_config); // Recreate the connection, since
+                                                  // the old one cannot be reused.
 
-//   safeConnection.connect(function(err) {              // The server is either down
-//     if(err) {                                     // or restarting (takes a while sometimes).
-//       console.log('error when connecting to db:', err);
-//       setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
-//     }                                     // to avoid a hot loop, and to allow our node script to
-//   });                                     // process asynchronous requests in the meantime.
-//                                           // If you're also serving http, display a 503 error.
-//   safeConnection.on('error', function(err) {
-//     console.log('db error', err);
-//     if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-//       handleDisconnect();                         // lost due to either server restart, or a
-//     } else {                                      // connnection idle timeout (the wait_timeout
-//       throw err;                                  // server variable configures this)
-//     }
-//   });
-// }
+  safeConnection.connect(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  safeConnection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
+    }
+  });
+}
 
-// handleDisconnect();
+handleDisconnect();
 
 /* GET home page. (this is for the express page, idk if we need this) */
 router.get('/', function(req, res, next) {
