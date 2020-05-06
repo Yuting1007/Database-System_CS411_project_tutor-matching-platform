9f90d1a56064f6a963f012bfa938905c2b6fb350
Nanyi Yang <nanyiyang@Nanyis-MacBook-Pro.local>
2020-05-05 21:07:57 -0700

    connection

--- react-backend/routes/index.js
+++ react-backend/routes/index.js
@@ -37,7 +37,7 @@ function handleDisconnect() {
   safeConnection.connect(function(err) {              // The server is either down
     if(err) {                                     // or restarting (takes a while sometimes).
       console.log('error when connecting to db:', err);
-      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+      setTimeout(handleDisconnect, 10000); // We introduce a delay before attempting to reconnect,
     }                                     // to avoid a hot loop, and to allow our node script to
   });                                     // process asynchronous requests in the meantime.
                                           // If you're also serving http, display a 503 error.
