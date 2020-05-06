77a7f271509be2aa5aef5ee190d87701a1d9c424
Nanyi Yang <nanyiyang@Nanyis-MacBook-Pro.local>
2020-05-05 20:56:58 -0700

    i hate heroku

--- react-backend/routes/start.js
+++ react-backend/routes/start.js
@@ -23,7 +23,38 @@ function getConnection() {
       //insecureAuth : true
       })
 }
+var db_config = {
+  host: 'us-cdbr-east-06.cleardb.net',
+    user: 'ba0144eebe0617',
+    password: '45188a1d', 
+    database: 'heroku_195486945502404'
+};
 
+var safeConnection;
+
+function handleDisconnect() {
+  console.log('START DISCONNECT');
+  safeConnection = mysql.createConnection(db_config); // Recreate the connection, since
+                                                  // the old one cannot be reused.
+
+  safeConnection.connect(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 10000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  safeConnection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
+    }
+  });
+}
+
+handleDisconnect();
 
 
 ////
